b310e1f9799148bcc339b44884867f8cf64e444e
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-04-14 16:13:29 -0700

    add AwesomeButton to Scoreboard

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { Container, Divider, Paper, Chip } from '@material-ui/core';
-import MakeBailButton from '../Buttons/MakeBailButton';
+import { Container, Paper } from '@material-ui/core';
+
+import { AwesomeButton } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
 
 export default class Scoreboard extends Component {
 	render() {
 		return (
 			<Paper elevation={2} style={{ textAlign: 'center', margin: 'auto' }}>
 				<Container>
-					<Chip />
-				</Container>
-				<Divider light />
-				<Container>
-					<MakeBailButton />
+					<AwesomeButton type="primary">S</AwesomeButton>
+					<AwesomeButton type="primary">K</AwesomeButton>
+					<AwesomeButton type="primary">A</AwesomeButton>
+					<AwesomeButton type="primary">T</AwesomeButton>
+					<AwesomeButton type="primary">E</AwesomeButton>
 				</Container>
 			</Paper>
 		);
