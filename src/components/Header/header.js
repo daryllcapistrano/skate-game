3dc556fb06fb8e6a771a427805015a3037d26720
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-04-15 22:02:46 -0700

    add header styles

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,21 +2,23 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { Container } from '@material-ui/core';
+
 const Header = ({ siteTitle }) => (
 	<header
 		style={{
-			background: `rebeccapurple`,
+			background: `black`,
 			marginBottom: `1.45rem`
 		}}
 	>
-		<div
+		<Container
 			style={{
 				margin: `0 auto`,
-				maxWidth: 960,
-				padding: `1.45rem 1.0875rem`
+				maxWidth: `960`,
+				padding: `1rem`
 			}}
 		>
-			<h1 style={{ margin: 0 }}>
+			<h2 style={{ margin: 0 }}>
 				<Link
 					to="/"
 					style={{
@@ -26,9 +28,11 @@ const Header = ({ siteTitle }) => (
 				>
 					{siteTitle}
 				</Link>
-				<p>a skateboarding game scoreboard</p>
-			</h1>
-		</div>
+				<Container>
+					<p style={{ color: `gray` }}> - game tracker</p>
+				</Container>
+			</h2>
+		</Container>
 	</header>
 );
 
