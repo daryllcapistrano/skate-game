308a749252df890cd4440cc7fb7bb222ddf5baf0
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-04-16 20:35:03 -0700

    make new pages for other games

---
+++ src/pages/add-on.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from '../components/Layout/layout';
+import SEO from '../components/seo';
+// My components
+// import Scoreboard from '../components/Scoreboard/Scoreboard';
+// import PlayerStats from '../components/playerstats';
+
+const AddOn = () => (
+	<Layout>
+		<SEO title="Home" />
+		{/* <Scoreboard />
+		<PlayerStats /> */}
+		<h1>add on page</h1>
+	</Layout>
+);
+
+export default AddOn;
---
+++ src/pages/spot-bible.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from '../components/Layout/layout';
+import SEO from '../components/seo';
+// My components
+// import Scoreboard from '../components/Scoreboard/Scoreboard';
+// import PlayerStats from '../components/playerstats';
+
+const SpotBible = () => (
+	<Layout>
+		<SEO title="Home" />
+		{/* <Scoreboard />
+		<PlayerStats /> */}
+		<h1>spot bible page</h1>
+	</Layout>
+);
+
+export default SpotBible;
