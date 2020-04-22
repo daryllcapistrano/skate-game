a8afc90d055ae5bcaef9148de8f20e06eb48b614
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-04-21 18:45:49 -0700

    add header links

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,12 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
 import { Link } from 'gatsby';
-
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -38,9 +34,6 @@ export default function Header({ siteTitle }) {
 		<div className={classes.root}>
 			<AppBar position="static" color="transparent">
 				<Toolbar>
-					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-						<MenuIcon />
-					</IconButton>
 					<Typography variant="h6" className={classes.title}>
 						<h2 style={{ margin: 0 }}>
 							<Link
@@ -57,7 +50,9 @@ export default function Header({ siteTitle }) {
 					<Container>
 						<p>a skateboard game tracker</p>
 					</Container>
-					<Button color="inherit">Login</Button>
+					<Link to="/add-on">Add On</Link>
+					<Link to="/trick-generator">Trick Generator</Link>
+					<Link to="/spot-bible">Spot Bible</Link>
 				</Toolbar>
 			</AppBar>
 		</div>
